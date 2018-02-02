6972d6e460d032fbaaf6b191eb684a76a86889b8
Me <cameronwandfluh@camerons-macbook-pro.local>
2018-02-01 19:57:51 -0600

    Initial Commit

---
+++ SwiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBasics
+//
+//  Created by Cameron Wandfluh on 2/1/18.
+//  Copyright © 2018 Cameron Wandfluh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
